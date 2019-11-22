7affee699f19dd0562bae80c805d9c7f0626373a
unknown <INFVA06671$@lntinfotech.com>
2019-11-22 12:37:33 +0530

    Jenkins-Infa-DB-147

--- MasterDB.sql
+++ MasterDB.sql
@@ -1,1 +1,11 @@
 --===================================================================
+--Commit ID  87b9816272c04e8649bc6137bb337f1237d817f8 On 22/November/2019 At 12:37:32 PM
+-- Query Start Area"
+CREATE TABLE "INFA_TGT"."LTI_TGT" 
+(	
+"FIRST_NAME" VARCHAR2(255 BYTE), 
+"LAST_NAME" VARCHAR2(255 BYTE)
+);
+-- Query End Area
+
+--===================================================================
--- Run.sql
+++ Run.sql
@@ -1,7 +1,7 @@
 SET TRIMSPOOL ON HEADING OFF ECHO OFF TERMOUT OFF UNDERLINE OFF PAGESIZE 0 LINESIZE 1000 TRIM ON
 whenever sqlerror exit sql.sqlcode;
 spool "C:\Program Files (x86)\Jenkins\workspace\Infa-DB\log.txt";
--- Place your query in the between the query area and ALWAYS APPEND THE CHANGES !!
+-- Place your query in the between the query area !!
 -- Query Start Area"
 CREATE TABLE "INFA_TGT"."LTI_TGT" 
 (	
