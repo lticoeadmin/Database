5cbaec1687daf912b0db4935ab492b90a6cab3de
Swapnil Kadam <Swapnil.Kadam@lntinfotech.com>
2020-03-02 15:35:46 +0530

    Added New column

--- Run.sql
+++ Run.sql
@@ -3,6 +3,6 @@ whenever sqlerror exit sql.sqlcode;
 spool "C:\Program Files (x86)\Jenkins\workspace\Infa-DB-Incremental\log.txt";
 -- Place your query in the between the query area !!
 -- Query Start Area"
-ALTER TABLE infa_tgt.lti_tgt add city varchar2(255);
+ALTER TABLE infa_tgt.lti_tgt add state varchar2(255);
 -- Query End Area
 spool off;
