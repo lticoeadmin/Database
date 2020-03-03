6dbdd73294040b4392835b41560993b1f26f1f46
Swapnil Kadam <Swapnil.Kadam@lntinfotech.com>
2020-03-03 19:49:41 +0530

    alter column

--- Run.sql
+++ Run.sql
@@ -4,6 +4,5 @@ spool "C:\Program Files (x86)\Jenkins\workspace\Infa-DB-Incremental\log.txt";
 -- Place your query in the between the query area !!
 -- Query Start Area"
 alter table INFA_COE_DEV.employee drop COLUMN created_user;
-alter table INFA_COE_DEV.employee drop COLUMN updated_user;
 -- Query End Area
 spool off;
