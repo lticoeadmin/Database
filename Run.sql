5631f8d9558e5267443dde55ce8bfacc11b18175
Swapnil Kadam <Swapnil.Kadam@lntinfotech.com>
2020-03-03 14:36:15 +0530

    added

--- Run.sql
+++ Run.sql
@@ -3,7 +3,7 @@ whenever sqlerror exit sql.sqlcode;
 spool "C:\Program Files (x86)\Jenkins\workspace\Infa-DB-Incremental\log.txt";
 -- Place your query in the between the query area !!
 -- Query Start Area"
-alter table infa_tgt.employee add created_date date;
-alter table infa_tgt.employee add updated_date date;
+alter table infa_tgt.employee add created_user varchar2(255);
+alter table infa_tgt.employee add updated_user varchar2(255);
 -- Query End Area
 spool off;
