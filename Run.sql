ebdab302d41db45b344812254d8ee02001001a6a
Swapnil Kadam <Swapnil.Kadam@lntinfotech.com>
2020-03-03 19:46:46 +0530

    removed audit columns

--- Run.sql
+++ Run.sql
@@ -3,7 +3,7 @@ whenever sqlerror exit sql.sqlcode;
 spool "C:\Program Files (x86)\Jenkins\workspace\Infa-DB-Incremental\log.txt";
 -- Place your query in the between the query area !!
 -- Query Start Area"
-alter table infa_tgt.employee add created_user varchar2(255);
-alter table infa_tgt.employee add updated_user varchar2(255);
+alter table INFA_COE_DEV.employee drop COLUMN created_user;
+alter table INFA_COE_DEV.employee drop COLUMN updated_user;
 -- Query End Area
 spool off;
