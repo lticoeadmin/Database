d4d8178c11020cb6159c8e4e83b7e3ceb341ad2f
Swapnil Kadam <Swapnil.Kadam@lntinfotech.com>
2020-03-03 10:43:20 +0530

    alter add column on employee

--- Run.sql
+++ Run.sql
@@ -3,13 +3,7 @@ whenever sqlerror exit sql.sqlcode;
 spool "C:\Program Files (x86)\Jenkins\workspace\Infa-DB-Incremental\log.txt";
 -- Place your query in the between the query area !!
 -- Query Start Area"
-create table infa_tgt.employee
-(
-emp_id int,
-emp_firstname varchar2(255),
-emp_lastname varchar2(255),
-gender varchar2(10),
-dob date
-);
+alter table infa_tgt.employee add created_date date;
+alter table infa_tgt.employee add updated_date date;
 -- Query End Area
 spool off;
